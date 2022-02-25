c47eba39e01d5ee914fede8e76194ae52fb31ced
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2022-02-26 02:40:52 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,60 +1,63 @@
-# import dash
+import dash
 import plotly.express as px
 import pandas as pd
-
-import dash
 from dash import Dash, html, dcc
 from dash.dependencies import Output, Input  # used in the section callback
 
 df = pd.read_csv("movies.csv")  # pandas dataframe
 
 
-# Page Layout
 
+# Page Layout
 app = dash.Dash(__name__)
 # Html layout
 app.layout = html.Div([
+    #setting up the headings for the page
+    #Division for Bar-Graph
 	html.Div([
     html.H1("User Data based Graphs in Python", style={
             'color': 'blue', 'fontSize': 40, 'textAlign': 'center'}),
+      html.H1("Created by Aditya Thapliyal 2016593", style={
+            'color': 'black', 'fontSize': 20, 'textAlign': 'left'}),
      html.H2("BAR-GRAPHS", style={
             'color': 'red', 'fontSize': 30, 'textAlign': 'center'}),
+
 
     dcc.Dropdown(id='Choice',
                  options=[{'label': x, 'value': x}
                           for x in (df.Genre.unique())],
-                 value='Romance'  # default value
+                 value='Comedy'  # default value
                  ),
     dcc.Graph(id='python-graph', figure={}),
-    # dcc.Graph(id='python-graph2', figure2={})#px.pie(data_frame=df, names="Occupation", values="Age")
+   ]),
 
-	]),
-
+    #Division for Pie Chart
     html.Div([
         
          html.H2("PIECHART", style={
             'color': 'red', 'fontSize': 30, 'textAlign': 'center'}),
-        dcc.Dropdown(id='Choice2',
-                     options=[{'label': x, 'value': x}
-                              for x in (df.Genre.unique())],
-                     value='Romance'  # default value
-                     ),
+        # dcc.Dropdown(id='Choice2',
+        #              options=[{'label': x, 'value': x}
+        #                       for x in (df.Genre.unique())],
+        #              value='Romance'  # default value
+        #              ),
         dcc.Graph(id='python-graph2', figure={}),
     ]),
 
 
     html.Div([
-       
+
+        #Division for Histogram
          html.H2("HISTOGRAM", style={
             'color': 'red', 'fontSize': 30, 'textAlign': 'center'}),
-        dcc.Dropdown(id='Choice3',
-                     options=[{'label': x, 'value': x}
-                              for x in (df.Genre.unique())],
-                     value='Romance'  # default value
-                     ),
+        # dcc.Dropdown(id='Choice3',
+        #              options=[{'label': x, 'value': x}
+        #                       for x in (df.Genre.unique())],
+        #              value='Romance'  # default value
+        #              ),
         dcc.Graph(id='python-graph3', figure={}),
     ])])
-    # dcc.Graph(id='python-graph2',
+
 
 
 @app.callback(
@@ -76,9 +79,8 @@ def interactive_graphing(value_genre):
 
 @app.callback(
     Output(component_id='python-graph2', component_property='figure'),
-    Input(component_id='Choice', component_property='value'),
-    # Output(component_id='python-graph2', component_property='figure2'),
-    # Input(component_id='Choice2', component_property='value')
+     Input(component_id='Choice', component_property='value'),
+    
 )  # callback decorator
 def interactive_graphing(value_genre):
     dff = df[df.Genre == value_genre]
@@ -89,12 +91,10 @@ def interactive_graphing(value_genre):
 @app.callback(
     Output(component_id='python-graph3', component_property='figure'),
     Input(component_id='Choice', component_property='value'),
-    # Output(component_id='python-graph2', component_property='figure2'),
-    # Input(component_id='Choice2', component_property='value')
 )  # callback decorator
 def interactive_graphing(value_genre):
     dff = df[df.Genre == value_genre]
-    fig3 = px.histogram(data_frame=dff,x='Audience score %', y='Genre')
+    fig3 = px.histogram(data_frame=dff,x='Audience score %')
    
     return fig3
         
@@ -116,23 +116,3 @@ if __name__ == '__main__':
 
 
 
-# print(df[:5]) 			   #print first 5 rows
-# print(df.iloc[:2,[1,2]]) # to print specief columns upto 5 rows?
-# print(df.age.nunique())
-# print(sorted(df.age.unique()))
-
-# Data visualization with plotly
-# fig_pie = px.pie(data_frame=df, names="Film", values="Budget",title="FIMS DATASET")
-# fig_pie.show()
-
-# fig_bar = px.bar(data_frame=df, x='gender', y="age")
-# fig_bar.show()
-
-# fig_hist = px.histogram(data_frame=df, x='gender', y='age')
-# fig_hist.show()
-
-# Interactive graphs with dash
-
-# import dash_html_components as html
-# import dash_core_componenets as dcc
-# from dash.dependencies import Output, Input
